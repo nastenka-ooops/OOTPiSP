80b3182daea6ed605061fc2dd430930b1b1727a2
nastenka-ooops <nastenka.brutskaya@mail.ru>
2024-03-27 09:10:02 +0300

    add JSON serialization/deserialization

---
+++ lapa12/src/main/java/com/example/lapa12/heros/Hilichurls.java
@@ -0,0 +1,9 @@
+package com.example.lapa12.heros;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hilichurls {
+
+    public List<Hilichurl> hilichurls = new ArrayList<>();
+}
